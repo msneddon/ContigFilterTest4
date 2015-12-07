f393cb169c5fc760e20c8277329405069573a32e
msneddon <mwsneddon@lbl.gov>
2015-12-07 14:41:52 -0800

    added autogenerated files

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 This repo is a simple example of a functional KBase module.  It defines one method that accepts a ContigSet and a length parameter, and creates a new ContigSet that has all contigs less than the specified length removed.
 
     kb-sdk init -e -u msneddon -l python MsneddonContigFilter
+
+    git init
+    git add .
+    git commit -m 'initial commit'
+    git remote add origin git@github.com:msneddon/ContigFilter
+    git push -u origin master
+
+    
--- lib/MsneddonContigFilter/MsneddonContigFilterImpl.py
+++ lib/MsneddonContigFilter/MsneddonContigFilterImpl.py
@@ -7,10 +7,12 @@ class MsneddonContigFilter:
     '''
     Module Name:
     MsneddonContigFilter
+
     Module Description:
-    
+    A KBase module: MsneddonContigFilter
+This sample module contains one small method - count_contigs.
     '''
-    
+
     ######## WARNING FOR GEVENT USERS #######
     # Since asynchronous IO can lead to methods - even the same method -
     # interrupting each other, you must be *very* careful when using global
@@ -20,7 +22,7 @@ class MsneddonContigFilter:
     #BEGIN_CLASS_HEADER
     workspaceURL = None
     #END_CLASS_HEADER
-    
+
     # config contains contents of config file in a hash or None if it couldn't
     # be found
     def __init__(self, config):
@@ -41,10 +43,10 @@ class MsneddonContigFilter:
             provenance = ctx['provenance']
         returnVal = {'contig_count': len(contigSet['contigs']), 'provenance': provenance}
         #END count_contigs
-        
+
         # At some point might do deeper type checking...
-        if not isinstance(returnVal, object):
+        if not isinstance(returnVal, dict):
             raise ValueError('Method count_contigs return value ' +
-                             'returnVal is not type object as required.')
+                             'returnVal is not type dict as required.')
         # return the results
         return [returnVal]
